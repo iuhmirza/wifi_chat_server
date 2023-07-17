ab4e0a6f3184eada0d1e6165955dacfc4e13293d
Ibtisam Mirza <iuhmirza@gmail.com>
2023-07-17 16:03:29 +0100

    Create mDNS server for visibility on LAN

--- go.mod
+++ go.mod
@@ -4,4 +4,10 @@ go 1.20
 
 require nhooyr.io/websocket v1.8.7
 
-require github.com/klauspost/compress v1.10.3 // indirect
+require (
+	github.com/hashicorp/mdns v1.0.5 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
+	github.com/miekg/dns v1.1.41 // indirect
+	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
+	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
+
+	"github.com/hashicorp/mdns"
 )
 
 func main() {
+	host, _ := os.Hostname()
+	service, err := mdns.NewMDNSService(
+		fmt.Sprintf("Wifi Chat - %v", host),
+		"_wifichat._tcp",
+		"",
+		"",
+		55555,
+		[]net.IP{},
+		[]string{"Wifi Chat Server"},
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	server, err := mdns.NewServer(&mdns.Config{Zone: service})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer server.Shutdown()
+
 	http.HandleFunc("/text", textHandler)
-	log.Println("Listening and serving on port 8080")
-	http.ListenAndServe(":8080", nil)
+	log.Println("Listening and serving on port 55555")
+	http.ListenAndServe(":55555", nil)
 }
